14926ad40cd79a7897d3046bebeb3773b180303b
DmEnd <Dima_polit-sov@mail.ru>
2022-11-10 02:41:06 +0300

    Добавьте файлы проекта.

---
+++ HomeWork14/Classes.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HomeWork14
+{
+    abstract class Animal
+    {
+        abstract public string Name { get; set; }
+        public Animal()
+        {
+            Name = "Животное";
+        }
+        abstract public void Say();
+        public void ShowInfo()
+        {
+            Console.Write("{0} ",Name);
+            Say();
+        }
+
+    }
+    class Cat : Animal
+    {
+        private string name;
+        public override string Name
+        {
+            get
+            {
+                return name;
+            }
+            set
+            {
+                name = value;
+            }
+        }
+        public override void Say()
+        {
+            Console.WriteLine("Мяу");
+        }
+    }
+    class Dog : Animal
+    {
+        private string name;
+        public override string Name
+        {
+            get
+            {
+                return name;
+            }
+            set
+            {
+                name = value;
+            }
+        }
+        public override void Say()
+        {
+            Console.WriteLine("Гав");
+        }
+    }
+}
---
+++ HomeWork14/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HomeWork14
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Cat cat= new Cat();
+            cat.ShowInfo();
+
+            cat.Name = "Мурка";
+            cat.ShowInfo();
+            cat.Say();
+
+
+            Dog dog = new Dog();
+            dog.ShowInfo();
+
+            dog.Name = "Шарик";
+            dog.ShowInfo();
+            dog.Say();
+            Console.ReadKey();
+        }
+    }
+}
